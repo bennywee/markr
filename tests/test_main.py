4ca05dd32bdd6774c59a2385639c77d5399fd420
Benjamin Wee <benw.cy93@gmail.com>
2023-05-25 10:47:10 +0000

    Mocked db connection and added extract xml test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,4 @@
-import json
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
 def test_extract_load_xml_error(client, missing_data, invalid_data):
     mising_response = client.post('/import/', data = missing_data)
@@ -7,7 +6,8 @@ def test_extract_load_xml_error(client, missing_data, invalid_data):
     assert mising_response.status_code == 400
     assert invalid_response.status_code == 400
 
-# def test_extract_load_xml(db, client, xml_data):
-#     with patch('app.main.extract_load_xml.sqlite3.connect') as db:
-#         response = client.post('/import/', data = xml_data)
-#         assert response.status_code == 200
+def test_extract_load_xml(db, client, xml_data):
+    with patch('sqlite3.connect', MagicMock(return_value=db)):
+        response = client.post('/import/', data = xml_data)
+        assert response.status_code == 200
+
