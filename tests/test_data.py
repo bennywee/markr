ca699667366ac3fa0806d349f400ed0e1aa95e4b
Benjamin Wee <benw.cy93@gmail.com>
2023-05-25 05:58:28 +0000

    Removed data and added assert statements

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,25 +1,13 @@
-import pytest
 import xml.etree.ElementTree as ET
-
 from app.data import parse_xml
-
-@pytest.fixture
-def xml_data():
-    xml_string = """
-    <mcq-test-results>
-        <mcq-test-result scanned-on="2017-12-04T12:12:10+11:00">
-            <first-name>Jane</first-name>
-            <last-name>Austen</last-name>
-            <student-number>521585128</student-number>
-            <test-id>1234</test-id>
-            <summary-marks available="20" obtained="13" />
-        </mcq-test-result>
-    </mcq-test-results>
-    """
-    return xml_string
 
 def test_parse_xml(xml_data):
     raw_data = ET.fromstring(xml_data)
-    data = parse_xml(result) for result in raw_data.findall('mcq-test-result')
-    ...
-    assert type(data) == dict
+    data = raw_data.findall('mcq-test-result')
+    parse_data = parse_xml(data[0])
+    assert type(parse_data) == dict
+    assert parse_data["scanned_datetime"] == "2017-12-04T12:12:10+11:00"
+    assert parse_data["student_number"] == "521585128"
+    assert parse_data["test_id"] == "1234"
+    assert parse_data["obtained_marks"] == "13"
+    assert parse_data["available_marks"] == "20"
