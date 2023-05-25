a81e2d5c46397466765ac97808c82456732e1187
Benjamin Wee <benw.cy93@gmail.com>
2023-05-25 07:17:50 +0000

    Test summary stats function

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,5 +1,5 @@
 import xml.etree.ElementTree as ET
-from app.data import parse_xml
+from app.data import parse_xml, stats
 
 def test_parse_xml(xml_data):
     raw_data = ET.fromstring(xml_data)
@@ -11,3 +11,14 @@ def test_parse_xml(xml_data):
     assert parse_data["test_id"] == "1234"
     assert parse_data["obtained_marks"] == "13"
     assert parse_data["available_marks"] == "20"
+
+def test_stats():
+    data = [1,2,3,4,5]
+    results = stats(data)
+    assert type(results) == dict
+    assert results['mean'] == 3.0
+    assert results['std'] == 1.4
+    assert results['p25'] == 2
+    assert results['p50'] == 3
+    assert results['p75'] == 4
+    assert results['count'] == 5 
