a8b0f2a8edc73a423684c58fe572ae9632b27180
Benjamin Wee <benw.cy93@gmail.com>
2023-05-27 17:49:30 +1000

    Updated fixture and patched database

--- tests/conftest.py
+++ tests/conftest.py
@@ -27,8 +27,8 @@ def db():
     scanned_datetime TEXT NOT NULL,
     student_number TEXT NOT NULL, 
     test_id TEXT NOT NULL,
-    obtained_marks INT,
-    available_marks INT,
+    obtained_marks INT CHECK(typeof(obtained_marks) = 'integer'),
+    available_marks INT CHECK(typeof(available_marks) = 'integer'),
     PRIMARY KEY (student_number, test_id)
     )
     """
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,21 @@
 from unittest.mock import MagicMock, patch
 
 
-def test_extract_load_xml_error(client, missing_data, invalid_data, scores_as_strings):
+def test_extract_load_missing(client, db, missing_data):
     with patch("sqlite3.connect", MagicMock(return_value=db)):
         missing_response = client.post("/import/", data=missing_data)
+        assert missing_response.status_code == 400
+
+
+def test_extract_load_invalid(client, db, invalid_data):
+    with patch("sqlite3.connect", MagicMock(return_value=db)):
         invalid_response = client.post("/import/", data=invalid_data)
-        string_response = client.post("/import/", data=scores_as_strings)
-        assert mising_response.status_code == 400
         assert invalid_response.status_code == 400
+
+
+def test_extract_load_score_strings(client, db, scores_as_strings):
+    with patch("sqlite3.connect", MagicMock(return_value=db)):
+        string_response = client.post("/import/", data=scores_as_strings)
         assert string_response.status_code == 400
 
 
